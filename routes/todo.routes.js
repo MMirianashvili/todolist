151b5b65086dbd85dbacdb11aaa6b063cd642d55
MMirianashvili <mykhailo.mirianashvili@tktk.ee>
2026-05-12 19:45:22 +0300

    todo api complete

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -1,50 +1,60 @@
 const express = require('express');
+const db = require('../db');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-
     try {
-        const data = db.query('SELECT * FROM todo;');
-        res.status(200).json({todo: data.rows});
-    }
-    catch(error) {
+        const data = await db.query('SELECT * FROM todo;');
+        res.status(200).json({ todo: data.rows });
+    } catch (error) {
         console.log(error);
     }
-    
 });
 
-router.post('/', (req, res) => {
-    console.log(req.body);
+router.post('/', async (req, res) => {
     const { task } = req.body;
 
     try {
-        const data = await db.query('INSERT INTO todo (task) VALUES ($1);', [task]);
-        console.log(data);
-        res.status(200).json({message: `${data.rowCount} row inserted.`});
-    } 
-    catch (error) {
-        console.log(error);
-    } 
+        const data = await db.query(
+            'INSERT INTO todo (task) VALUES ($1);',
+            [task]
+        );
 
+        res.status(200).json({
+            message: `${data.rowCount} row inserted.`
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 router.delete('/', async (req, res) => {
-    const {id} = req;
-    const data = await db.query("SELECT * FROM todo WHERE id = $1;", [id]);
+    const { id } = req.body;
 
-    if(data.rows.length === 0) {
-        res.json({message: "there no such task"});
+    const data = await db.query(
+        "SELECT * FROM todo WHERE id = $1;",
+        [id]
+    );
+
+    if (data.rows.length === 0) {
+        res.json({ message: "there no such task" });
     } else {
         try {
-            const result = await db.query("DELETE FROM todo WHERE id = $1;", [id]);
-            res.status(200).json({message: `${result.rowCount} row was deleted.`});
-        }
-        catch(error) {
+            const result = await db.query(
+                "DELETE FROM todo WHERE id = $1;",
+                [id]
+            );
+
+            res.status(200).json({
+                message: `${result.rowCount} row was deleted.`
+            });
+
+        } catch (error) {
             console.log(error);
         }
     }
 });
 
-
-module.exports = ; 
+module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const todoRouter = require();
+const todoRouter = require('./routes/todo.routes');
 const dotenv = require('dotenv');
+
+dotenv.config();
+
 const app = express();
 
 app.use(bodyParser.json());
 
+app.use('/', todoRouter);
 
-app.use('/', );
-
-app.listen(, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running on Port 3000.');
 });
