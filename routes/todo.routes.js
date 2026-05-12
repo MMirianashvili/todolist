67d82db0c8f53df1b7ada2485950d4c869532fd8
MMirianashvili <mykhailo.mirianashvili@tktk.ee>
2026-05-12 21:42:20 +0300

    fix routes

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -7,24 +7,20 @@ router.get('/', async (req, res) => {
     try {
         const data = await db.query('SELECT * FROM primarytable;');
 
-        res.status(200).json({
-            primarytable: data.rows
-        });
-
+        res.status(200).json(data.rows);
     } catch (error) {
         console.log(error);
+        res.status(500).json({ error: error.message });
     }
 });
 
 router.post('/', async (req, res) => {
-
-    const { task } = req.body;
+    const { key, value } = req.body;
 
     try {
-
         const data = await db.query(
             'INSERT INTO primarytable (key, value) VALUES ($1, $2);',
-            ['1', task]
+            [key, value]
         );
 
         res.status(200).json({
@@ -33,40 +29,26 @@ router.post('/', async (req, res) => {
 
     } catch (error) {
         console.log(error);
+        res.status(500).json({ error: error.message });
     }
 });
 
 router.delete('/', async (req, res) => {
-
     const { key } = req.body;
 
-    const data = await db.query(
-        'SELECT * FROM primarytable WHERE key = $1;',
-        [key]
-    );
+    try {
+        const data = await db.query(
+            'DELETE FROM primarytable WHERE key = $1;',
+            [key]
+        );
 
-    if (data.rows.length === 0) {
-
-        res.json({
-            message: 'there no such task'
+        res.status(200).json({
+            message: `${data.rowCount} row deleted.`
         });
 
-    } else {
-
-        try {
-
-            const result = await db.query(
-                'DELETE FROM primarytable WHERE key = $1;',
-                [key]
-            );
-
-            res.status(200).json({
-                message: `${result.rowCount} row was deleted.`
-            });
-
-        } catch (error) {
-            console.log(error);
-        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
     }
 });
 
